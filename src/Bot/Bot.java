78fb00dfdaf948a23aa86e838aa32ce7734542e4
Kayk Caputo <kayk.caputo@gmail.com>
2023-05-03 11:19:47 -0300

    pequenas correções

--- src/Bot/Bot.java
+++ src/Bot/Bot.java
@@ -7,7 +7,7 @@ public class Bot {
     public static final ArrayList<String> DuvidaLogar = new ArrayList<>(List.of(new String[]{"como faço para logar?", "como faço login?"}));
     public static final ArrayList<String> DuvidaNotas = new ArrayList<>(List.of(new String[]{"como vejo minhas notas?", "onde eu vejo minhas notas?"}));
     public static Duvidas comoLogar = new Duvidas(DuvidaLogar, "Não sei bro, vtmnc, todo dia isso namoral");
-    public static Duvidas comoVerNotas = new Duvidas(DuvidaLogar, "Não sei bro, vtmnc, todo dia isso namoral");
+    public static Duvidas comoVerNotas = new Duvidas(DuvidaNotas, "Não sei bro, vtmnc, todo dia isso namoral");
 
     public static void responde(String duvida){
         duvida = duvida.toLowerCase();
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Bot.Bot;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Olá sou o (insira um bom nome de bot aqui), o unico motivo da minha triste e limitada existencia é auxilialo a utilizar o SIGAA, qual a sua duvida?\n");
+        System.out.println("Olá sou o (insira um bom nome de bot aqui), o unico motivo da minha triste e limitada existencia é auxilia-lo a utilizar o SIGAA, qual a sua duvida?");
         Bot.responde(input.nextLine());
     }
 }
